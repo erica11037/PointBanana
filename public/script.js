742943c642f9630355a42002d974fd800a38cf8a
erica11037 <mytt.erica@gmail.com>
2026-05-31 22:04:43 -0300

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,27 @@
+var cont = 1;
+document.getElementById('radio1').checked = true;
+
+setInterval(() => {
+    proximaImg();
+}, 10000);
+
+function proximaImg() {
+    cont++;
+    if (cont > 5) {
+        cont = 1;
+    }
+    document.getElementById('radio' + cont).checked = true;
+}
+
+// isso aqui atualiza o contador se você clicar nas bolinhas manualmente
+document.querySelectorAll('input[name="botao__radio"]').forEach((elem, index) => {
+    elem.addEventListener("change", function() {
+        cont = index + 1;
+    });
+});
+
+//LEMBRETE: estudar dps para conseguir compreender melhor o código
+
+
+//-------------------FAVORITAR ITEM
+
